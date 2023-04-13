1bcaf168d1480733e2cabe59f271c4eb8a0aef83
Finene <finene@stellarcapital.org>
2023-04-14 05:05:59 +0900

    すだち

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -3,78 +3,70 @@
 class StatusesIndex < Chewy::Index
   include FormattingHelper
 
-  settings index: { refresh_interval: '30s' }, analysis: {
-    filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
-      },
-    },
-    analyzer: {
-      content: {
-        tokenizer: 'uax_url_email',
-        filter: %w(
-          english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
-          english_stemmer
-        ),
-      },
-    },
-  }
-
-  # We do not use delete_if option here because it would call a method that we
-  # expect to be called with crutches without crutches, causing n+1 queries
+  settings index: { refresh_interval: '30s' },
+           analysis: {
+             tokenizer: {
+               sudachi_tokenizer: {
+                 type: 'sudachi_tokenizer',
+                 discard_punctuation: true,
+                 resources_path: 'C:/elasticsearch/config/sudachi/',
+                 settings_path: 'C:/elasticsearch/config/sudachi/sudachi.json',
+               },
+             },
+             analyzer: {
+               content: {
+                 filter: %w(
+           lowercase
+           cjk_width
+          sudachi_part_of_speech
+          sudachi_ja_stop
+          sudachi_baseform
+         ),
+                 tokenizer: 'sudachi_tokenizer',
+                 type: 'custom',
+               },
+             },
+           }
+  #        settings_path: 'C:/elasticsearch/config/sudachi/sudachi.json',
   index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :preloadable_poll)
 
   crutch :mentions do |collection|
-    data = ::Mention.where(status_id: collection.map(&:id)).where(account: Account.local, silent: false).pluck(:status_id, :account_id)
+    data = ::Mention.where(status_id: collection.map(&:id)).pluck(:status_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
 
   crutch :favourites do |collection|
-    data = ::Favourite.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
+    data = ::Favourite.where(status_id: collection.map(&:id)).pluck(:status_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
 
   crutch :reactions do |collection|
-    data = ::Reaction.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
+    data = ::Reaction.where(status_id: collection.map(&:id)).pluck(:status_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
 
   crutch :reblogs do |collection|
-    data = ::Status.where(reblog_of_id: collection.map(&:id)).where(account: Account.local).pluck(:reblog_of_id, :account_id)
+    data = ::Status.where(reblog_of_id: collection.map(&:id)).pluck(:reblog_of_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
 
   crutch :bookmarks do |collection|
-    data = ::Bookmark.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
+    data = ::Bookmark.where(status_id: collection.map(&:id)).pluck(:status_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
-
   crutch :votes do |collection|
-    data = ::PollVote.joins(:poll).where(poll: { status_id: collection.map(&:id) }).where(account: Account.local).pluck(:status_id, :account_id)
+    data = ::PollVote.joins(:poll).where(poll: { status_id: collection.map(&:id) }).pluck(:status_id, :account_id)
     data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
   end
-
   root date_detection: false do
     field :id, type: 'long'
     field :account_id, type: 'long'
 
-    field :text, type: 'text', value: ->(status) { status.searchable_text } do
+    field :text, type: 'text', analyzer: 'content', value: ->(status) { [status.spoiler_text, PlainTextFormatter.new(status.text, status.local?).to_s].concat(status.media_attachments.map(&:description)).concat(status.preloadable_poll ? status.preloadable_poll.options : []).join("\n\n") } do
       field :stemmed, type: 'text', analyzer: 'content'
     end
 
     field :searchable_by, type: 'long', value: ->(status, crutches) { status.searchable_by(crutches) }
+    field :created_at, type: 'date'
   end
 end
